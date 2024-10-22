41a049ef2f546db88bd0654cf8f7550ec430dbf3
Jake Dahlgren <121595531+JAKEDAHLGREN@users.noreply.github.com>
2024-10-22 09:22:49 -0600

    Created a new, edit, and update method, updated the necessary routes, and put the form into a partial and used locals to render properly based on action

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     # blog_post_path(1) -> "/blog_posts/1" or blog_post_url(1) -> "http://localhost:300/blog_posts/1"
     get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
     get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+    patch "/blog_posts/:id",to: "blog_posts#update"
+    get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
     post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   root "blog_posts#index"
