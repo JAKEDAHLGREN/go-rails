450dd966daafcb7ea0b74e1af76309520f468323
Jake Dahlgren <121595531+JAKEDAHLGREN@users.noreply.github.com>
2024-10-23 09:45:05 -0600

    Added Devise w/views, updated blog_post controller, updated button styles, added destroy action, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Resource builds all crud action routes for blog
   # Using "AS" in the route wil create a variable for you to use in your code
     # :blog_post can reference two separate routes:
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
     get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
     get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
     patch "/blog_posts/:id",to: "blog_posts#update"
+    delete "/blog_posts/:id",to: "blog_posts#destroy"
     get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
     post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
